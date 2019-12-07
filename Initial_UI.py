54993b91f65adc7630c7176d9218e0547bfffc4f
dilano55 <dilano@tranga.com>
2019-12-07 18:30:31 +0100

    made a frame for the second screen

--- Initial_UI.py
+++ Initial_UI.py
@@ -2,34 +2,25 @@ import tkinter as ui
 import re
 import time
 # window settings_START
-x = "ok"
 window = ui.Tk()
 window.title("Start")
-window.configure(bg="black")
-screen = ui.Canvas(window, width=400, height=150,
-                   bg="black", bd=0, highlightthickness=0)
+window.configure(bg="grey")
+screen = ui.Canvas(window, width=400, height=150,bg="grey", bd=0, highlightthickness=0)
 screen.pack()
-# second window settings
-window_2 = ui.Tk()
-window_2.title("Layout")
-window_2.geometry("400x400")
-window_2.configure(bg="black")
 # variables:_START
 amount_kids = 0
 amount_beds = 0
 # widgets_1_START
-text2 = ui.Label(screen, text='Amount of children?', fg="white",
-                 bg="black", font=("arial", 20), width=15)
-text3 = ui.Label(screen, text='Amount of beds?', fg="white",
-                 bg="black", font=("arial", 20), width=15)
+text2 = ui.Label(screen, text='Amount of children?',font=("arial", 20), width=15, bg="grey")
+text3 = ui.Label(screen, text='Amount of beds?', font=("arial", 20), width=15, bg="grey")
 amount_of_children = ui.Entry(screen, width=3)
 amount_of_beds = ui.Entry(screen, width=3)
 text_error = ui.Label(window, text="Please enter valid values", bg="red")
-text_error_2 = ui.Label(
-    window, text="Please enter values greater than 0", bg="red")
+text_error_2 = ui.Label(window, text="Please enter values greater than 0", bg="red")
+
+#CANVAS LAYOUT
+layout_beds = ui.Frame(window,bg="grey",width = 500, height = 500)
 # functions_2_START
-
-
 def note():
     var = str(amount_of_children.get())
     var_2 = str(amount_of_children.get())
@@ -39,7 +30,9 @@ def note():
         text_error_2.pack_forget()
         text_error.pack_forget()
         time.sleep(0.3)
-        window.destroy()
+        screen.pack_forget()
+        layout_beds.pack()
+        
         if int(amount_beds) > 0 and int(amount_kids) > 0:
             print(amount_kids, amount_beds)
         elif int(amount_beds) <= 0 or int(amount_kids) <= 0:
@@ -56,10 +49,12 @@ def exit():
     window.quit()
 
 
-# widgets_2_START
+# widgets_2_START(quit and confirm buttons)
 confirm_button = ui.Button(screen, text="Confirm", command=note, width=6)
 quit_button = ui.Button(screen, text="Quit", command=exit, width=6)
+quit_button2 = ui.Button(layout_beds, text="Quit", command=exit, width=6)
 # placements_START
+quit_button2.place(x=230,y=478)
 quit_button.place(x=185, y=125)
 confirm_button.place(x=185, y=100)
 text2.place(y=10, x=4)
@@ -67,6 +62,4 @@ text3.place(y=50, x=4)
 amount_of_children.place(y=20, x=300)
 amount_of_beds.place(y=60, x=300)
 # window1_START
-window.mainloop()
-
-# window 2 start
+ui.mainloop()
